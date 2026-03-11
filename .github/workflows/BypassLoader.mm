305dd8051da8a634110ff00101f6b902065e4f63
Gh0st077 <abelfamilia60@gmail.com>
2026-03-11 05:14:06 -0400

    Add BypassLoader.mm for dynamic library loading

---
+++ .github/workflows/BypassLoader.mm
@@ -0,0 +1,13 @@
+#import <Foundation/Foundation.h>
+#import <dlfcn.h>
+#import <mach-o/dyld.h>
+
+void cargarMod() {
+    [NSThread sleepForTimeInterval:8];
+    NSString *dylibPath = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"UnityAnalyticsCore.dylib"];
+    dlopen([dylibPath UTF8String], RTLD_NOW);
+}
+
+__attribute__((constructor)) static void init() {
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{ cargarMod(); });
+}
